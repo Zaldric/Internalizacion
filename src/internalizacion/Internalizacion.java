6c808f4f4404aec2cb9284728ffeb2d406d99a54
Zaldric <zaldric.jck@gmail.com>
2016-11-11 09:56:06 +0100

    Lectura del fichero y carga de las estructuras hecho

--- src/internalizacion/Internalizacion.java
+++ src/internalizacion/Internalizacion.java
@@ -5,17 +5,113 @@
  */
 package internalizacion;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+ /*
+    Formato del fichero
+    2
+    EN
+    ES
+    Inglés 
+    Español
+    11
+    Aceptar
+    Cancelar
+    ....
+    11
+    Ok 
+    Cancel
+    3
+    img/presidente.jpg
+    img/crack.jpg
+    img/hola.jpg
+    3
+    .....
+    img/english.jpg
+    Formato: cadena[idioma][pos]
+    */
+
 /**
  *
  * @author Juanca
  */
 public class Internalizacion {
+    
+    private static int x, y, z;
+    private static ArrayList<String> idiomas;
+    private static ArrayList<String> imagenes;
+    private static String datos[][];
+    
+    public static void leerFichero(String fich) throws FicheroNoEncontrado {
+        if (!(new File(fich)).exists()) {
+            throw new FicheroNoEncontrado("Fichero no encontrado \n");
+        }
+        File archivo;
+        FileReader fr = null;
+        BufferedReader br;
+        try {
+            archivo = new File(fich);
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+            String texto;
+            texto = br.readLine();
+            x = Integer.parseInt(texto);
+            
+            for(int i = 0; i < x * 2; ++i){
+                texto = br.readLine();
+                idiomas.add(texto);
+            }
+            
+            texto = br.readLine();
+            y = Integer.parseInt(texto);
+
+            datos = new String[x][y];
+
+            for (int i = 0; i < x; ++i) {
+                for (int j = 0; j < y; ++j) {
+                    texto = br.readLine();
+                    datos[i][j] = texto;
+                }
+                z = Integer.parseInt(br.readLine());
+            }
+            
+            
+            for (int i = 0; i < z; ++i){
+                texto = br.readLine();
+                imagenes.add(texto);
+            }
+            
+            z = Integer.parseInt(br.readLine());
+            
+            for (int i = 0; i < z; ++i){
+                texto = br.readLine();
+                imagenes.add(texto);
+            }
+            
+        } catch (IOException | NumberFormatException e) {
+        } finally {
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+            }
+        }
+    }
 
     /**
      * @param args the command line arguments
+     * @throws internalizacion.FicheroNoEncontrado
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FicheroNoEncontrado {
         // TODO code application logic here
+        idiomas = new ArrayList<>();
+        imagenes = new ArrayList<>();
+        leerFichero("idiomas.txt");
     }
     
 }
